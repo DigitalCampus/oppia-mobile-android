6f888fbd87f76c1216a32eb6e95f962eb549fa79
Alex Little <alex@alexlittle.net>
2012-08-07 00:38:45 +0200

    Add background gradient

--- src/org/digitalcampus/mtrain/activity/ModuleActivity.java
+++ src/org/digitalcampus/mtrain/activity/ModuleActivity.java
@@ -8,10 +8,12 @@ import org.digitalcampus.mtrain.R.menu;
 import org.digitalcampus.mtrain.model.Module;
 import org.digitalcampus.mtrain.model.Section;
 import org.digitalcampus.mtrain.widgets.PageWidget;
+import org.digitalcampus.mtrain.widgets.QuizWidget;
 import org.digitalcampus.mtrain.widgets.WidgetFactory;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector.OnGestureListener;
@@ -19,6 +21,8 @@ import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
@@ -62,14 +66,37 @@ public class ModuleActivity extends Activity {
     	if(acts.get(this.currentActivityNo).getActType().equals("page")){
     		currentActivity = (PageWidget) new PageWidget(ModuleActivity.this, module, acts.get(this.currentActivityNo).getActivity());
     	}
+    	if(acts.get(this.currentActivityNo).getActType().equals("quiz")){
+    		currentActivity = (QuizWidget) new QuizWidget(ModuleActivity.this, module, acts.get(this.currentActivityNo).getActivity());
+    	}
     	this.setUpNav();
     }
     
     private void setUpNav(){
+    	Button prevB = (Button) ModuleActivity.this.findViewById(R.id.prev_btn);
+    	Button nextB = (Button) ModuleActivity.this.findViewById(R.id.next_btn);
     	if(this.hasPrev()){
-    		
+    		prevB.setEnabled(true);
+    		prevB.setOnClickListener(new View.OnClickListener() {
+             	public void onClick(View v) {
+             		currentActivityNo--;
+             		loadActivity();
+             	}
+             });
     	} else {
-    		
+    		prevB.setEnabled(false);
+    	}
+    	
+    	if(this.hasNext()){
+    		nextB.setEnabled(true);
+    		nextB.setOnClickListener(new View.OnClickListener() {
+             	public void onClick(View v) {
+             		currentActivityNo++;
+             		loadActivity();
+             	}
+             });
+    	} else {
+    		nextB.setEnabled(false);
     	}
     }
     
@@ -80,7 +107,12 @@ public class ModuleActivity extends Activity {
     	return true;
     }
     private boolean hasNext(){
-    	return true;
+    	int noActs = section.getActivities().size();
+    	if(this.currentActivityNo + 1 == noActs){
+    		return false;
+    	} else {
+    		return true;
+    	}
     }
     
 }
