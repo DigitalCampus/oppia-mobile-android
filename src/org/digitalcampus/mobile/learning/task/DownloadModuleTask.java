44fc2ce13bcb87798808cc3cf833981c04387c2c
Alex Little <alex@alexlittle.net>
2012-11-27 17:38:04 +0100

    Update to use the new API

--- src/org/digitalcampus/mobile/learning/task/DownloadModuleTask.java
+++ src/org/digitalcampus/mobile/learning/task/DownloadModuleTask.java
@@ -3,10 +3,18 @@ package org.digitalcampus.mobile.learning.task;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.digitalcampus.mobile.learning.application.MobileLearning;
 import org.digitalcampus.mobile.learning.listener.InstallModuleListener;
 import org.digitalcampus.mobile.learning.model.Module;
@@ -15,7 +23,9 @@ import org.digitalcampus.mobile.learning.R;
 import com.bugsense.trace.BugSenseHandler;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class DownloadModuleTask extends AsyncTask<Payload, Object, Payload>{
@@ -23,9 +33,11 @@ public class DownloadModuleTask extends AsyncTask<Payload, Object, Payload>{
 	private final static String TAG = "DownloadModuleTask";
 	private InstallModuleListener mStateListener;
 	private Context ctx;
+	private SharedPreferences prefs;
 	
 	public DownloadModuleTask(Context ctx) {
 		this.ctx = ctx;
+		prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
 	}
 	
 	@Override
@@ -33,11 +45,41 @@ public class DownloadModuleTask extends AsyncTask<Payload, Object, Payload>{
 		// TODO what to do when there is an error connecting - how to flag back to user
 		for (Payload payload : params) {
 			Module dm = (Module) payload.data[0];
-			Log.d(TAG,"Downloading:" + dm.getDownloadUrl());
+
 			try { 
+				HttpParams httpParameters = new BasicHttpParams();
+				HttpConnectionParams.setConnectionTimeout(
+						httpParameters,
+						Integer.parseInt(prefs.getString("prefServerTimeoutConnection",
+								ctx.getString(R.string.prefServerTimeoutConnection))));
+				HttpConnectionParams.setSoTimeout(
+						httpParameters,
+						Integer.parseInt(prefs.getString("prefServerTimeoutResponse",
+								ctx.getString(R.string.prefServerTimeoutResponseDefault))));
+				DefaultHttpClient client = new DefaultHttpClient(httpParameters);
+
+				// add api_key/username params
+				List<NameValuePair> pairs = new LinkedList<NameValuePair>();
+				pairs.add(new BasicNameValuePair("username", prefs.getString("prefUsername", "")));
+				pairs.add(new BasicNameValuePair("api_key", prefs.getString("prefApiKey", "")));
+				String paramString = URLEncodedUtils.format(pairs, "utf-8");
+				
+				String url = dm.getDownloadUrl();
+				
+				if(!url.endsWith("?"))
+			        url += "?";
+				url += paramString;
+				
+				Log.d(TAG,"Downloading:" + url);
+				
+				HttpGet httpGet = new HttpGet(url);
+				
 				String localFileName = dm.getShortname()+"-"+String.format("%.0f",dm.getVersionId())+".zip";
 				Log.d(TAG,"saving to: "+localFileName);
-				new DefaultHttpClient().execute(new HttpGet(dm.getDownloadUrl())).getEntity().writeTo(new FileOutputStream(new File(MobileLearning.DOWNLOAD_PATH,localFileName)));
+				
+				FileOutputStream fos = new FileOutputStream(new File(MobileLearning.DOWNLOAD_PATH,localFileName));
+				client.execute(httpGet).getEntity().writeTo(fos);
+				fos.close();
 				publishProgress(ctx.getString(R.string.download_complete));
 			} catch (ClientProtocolException e1) { 
 				e1.printStackTrace(); 
@@ -46,7 +88,6 @@ public class DownloadModuleTask extends AsyncTask<Payload, Object, Payload>{
 				e1.printStackTrace();
 				BugSenseHandler.log(TAG, e1);
 			}
-			 
 		}
 		return null;
 	}
