0a605c5b896f78e8510c080764b8d8d3ba35142f
alexlittle <alex@alexlittle.net>
2014-03-21 18:57:10 +0100

    OPEN - task 277: Login/Register - merge into one activity with fragments https://github.com/DigitalCampus/oppia-mobile-android/issues/issue/277

---
+++ src/org/digitalcampus/oppia/fragments/LoginFragment.java
@@ -0,0 +1,76 @@
+/* 
+ * This file is part of OppiaMobile - http://oppia-mobile.org/
+ * 
+ * OppiaMobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OppiaMobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with OppiaMobile. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.digitalcampus.oppia.fragments;
+
+import java.util.Locale;
+
+import org.digitalcampus.mobile.learning.R;
+import org.digitalcampus.oppia.application.MobileLearning;
+import org.digitalcampus.oppia.utils.FileUtils;
+
+import com.bugsense.trace.BugSenseHandler;
+
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
+
+public class LoginFragment extends Fragment{
+
+
+	public static final String TAG = LoginFragment.class.getSimpleName();
+	private WebView webView;
+	private SharedPreferences prefs;
+	
+	public static LoginFragment newInstance() {
+		LoginFragment myFragment = new LoginFragment();
+	    return myFragment;
+	}
+
+	public LoginFragment(){
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		prefs = PreferenceManager.getDefaultSharedPreferences(super.getActivity());
+		View vv = super.getLayoutInflater(savedInstanceState).inflate(R.layout.fragment_about, null);
+		LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		vv.setLayoutParams(lp);
+		return vv;
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+
+	}
+}
