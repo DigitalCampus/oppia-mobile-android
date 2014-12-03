877174cfb8c8b99fe13b2cb9dc6f3254abeae9ab
alexlittle <alex@alexlittle.net>
2014-12-03 14:55:46 -0500

    update link in license header

--- src/org/digitalcampus/oppia/task/RegisterTask.java
+++ src/org/digitalcampus/oppia/task/RegisterTask.java
@@ -1,5 +1,5 @@
 /* 
- * This file is part of OppiaMobile - http://oppia-mobile.org/
+ * This file is part of OppiaMobile - https://digital-campus.org/
  * 
  * OppiaMobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
