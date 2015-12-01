2b2ed8e8bcd39a5ea32437c62f0cd4e04b86fdd4
Joseba <jjoseba@gmail.com>
2015-12-01 15:32:52 +0100

    Finish help dialog for the Lollipop Storage Access framework

--- src/org/digitalcampus/oppia/utils/storage/GrantStorageAccessFragment.java
+++ src/org/digitalcampus/oppia/utils/storage/GrantStorageAccessFragment.java
@@ -1,24 +1,43 @@
 package org.digitalcampus.oppia.utils.storage;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class GrantStorageAccessFragment extends Fragment {
+import org.digitalcampus.mobile.learning.R;
+import org.digitalcampus.oppia.listener.ListInnerBtnOnClickListener;
+
+public class GrantStorageAccessFragment extends Fragment implements ListInnerBtnOnClickListener {
 
     private static final int REQUEST_GRANT_CODE = 12;
+    private static final int RESULT_OK = 1;
+    private static final int RESULT_DISMISS = -1;
     public static final String FRAGMENT_TAG = "GrantStorageAccessFragment";
+
+    private InfoDialog dialog;
 
     public interface AccessGrantedListener{
         void onAccessGranted(Uri pathAccessGranted);
     }
+
     private AccessGrantedListener listener;
-
-
     public void setListener(AccessGrantedListener listener){
         this.listener = listener;
     }
@@ -26,10 +45,21 @@ public class GrantStorageAccessFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final Intent intent;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-            this.startActivityForResult(intent, REQUEST_GRANT_CODE);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        dialog = new InfoDialog();
+        dialog.setListener(this);
+        dialog.show(fragmentManager, "dialog");
+    }
+
+    @Override
+    public void onClick(int result) {
+        if (result == RESULT_DISMISS){
+            if (listener != null) listener.onAccessGranted(null);
+        }
+        else if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            startActivityForResult(intent, REQUEST_GRANT_CODE);
         }
     }
 
@@ -38,6 +68,7 @@ public class GrantStorageAccessFragment extends Fragment {
         if (requestCode == REQUEST_GRANT_CODE && resultCode == Activity.RESULT_OK) {
             Uri treeUri = resultData.getData();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
                 final ContentResolver resolver = getActivity().getContentResolver();
                 resolver.takePersistableUriPermission(treeUri,
                     Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
@@ -48,4 +79,62 @@ public class GrantStorageAccessFragment extends Fragment {
             listener.onAccessGranted(null);
         }
     }
+
+    public static class InfoDialog extends DialogFragment implements View.OnClickListener {
+
+        public InfoDialog(){ }
+
+        private ListInnerBtnOnClickListener listener;
+        public void setListener(ListInnerBtnOnClickListener listener){
+            this.listener = listener;
+        }
+        private boolean intentSent = false;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            View view = inflater.inflate(R.layout.dialog_lollipop_storage_access, null);
+            Button acceptButton = (Button) view.findViewById(R.id.acceptBtn);
+
+            acceptButton.setOnClickListener(this);
+            getDialog().setCanceledOnTouchOutside(true);
+            return view;
+        }
+
+        public void onResume()
+        {
+            Window window = getDialog().getWindow();
+            final WindowManager.LayoutParams attrs = window.getAttributes();
+
+            DisplayMetrics metrics = new DisplayMetrics();
+            WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            display.getMetrics(metrics);
+
+            attrs.gravity = Gravity.CENTER;
+            attrs.height = (int) (metrics.heightPixels * 0.8);
+            attrs.width = (int) (metrics.widthPixels * 0.8);
+            window.setAttributes(attrs);
+
+            super.onResume();
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            if (!intentSent && (listener!=null))
+                listener.onClick(RESULT_DISMISS);
+            listener = null;
+            super.onCancel(dialog);
+        }
+
+        @Override
+        public void onClick(View v) {
+            intentSent = true;
+            getDialog().cancel();
+            if (listener!=null) listener.onClick(RESULT_OK);
+        }
+    }
+
+
 }
