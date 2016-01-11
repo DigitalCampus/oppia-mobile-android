43a1fdfd61f5555586ffdca1e3d758d7a2f3d255
Joseba <jjoseba@gmail.com>
2016-01-11 20:20:40 +0100

    Made that a click in a piechart in the scorecard grid takes the user to that course index page.

--- src/org/digitalcampus/oppia/fragments/GlobalScorecardFragment.java
+++ src/org/digitalcampus/oppia/fragments/GlobalScorecardFragment.java
@@ -1,15 +1,17 @@
 package org.digitalcampus.oppia.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 
 import org.digitalcampus.mobile.learning.R;
+import org.digitalcampus.oppia.activity.CourseIndexActivity;
 import org.digitalcampus.oppia.adapter.ScorecardListAdapter;
 import org.digitalcampus.oppia.application.DatabaseManager;
 import org.digitalcampus.oppia.application.DbHelper;
@@ -18,9 +20,10 @@ import org.digitalcampus.oppia.model.Course;
 
 import java.util.ArrayList;
 
-public class GlobalScorecardFragment extends Fragment {
+public class GlobalScorecardFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     public static final String TAG = CourseScorecardFragment.class.getSimpleName();
+    private ScorecardListAdapter scorecardListAdapter;
 
     public static GlobalScorecardFragment newInstance() {
         return new GlobalScorecardFragment();
@@ -48,8 +51,21 @@ public class GlobalScorecardFragment extends Fragment {
         long userId = db.getUserId(SessionManager.getUsername(getActivity()));
         ArrayList<Course> courses = db.getCourses(userId);
         DatabaseManager.getInstance().closeDatabase();
-        ScorecardListAdapter scorecardListAdapter = new ScorecardListAdapter(super.getActivity(), courses);
+        scorecardListAdapter = new ScorecardListAdapter(super.getActivity(), courses);
         GridView scorecardList = (GridView) super.getActivity().findViewById(R.id.scorecards_list);
         scorecardList.setAdapter(scorecardListAdapter);
+        scorecardList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Course selectedCourse = scorecardListAdapter.getItem(position);
+        Intent i = new Intent(super.getActivity(), CourseIndexActivity.class);
+        Bundle tb = new Bundle();
+        tb.putSerializable(Course.TAG, selectedCourse);
+        i.putExtras(tb);
+        startActivity(i);
+        super.getActivity().finish();
     }
 }
