797de449af4e6b1aba0149e2541bc4f443e09f71
Joseba <jjoseba@gmail.com>
2016-02-10 16:52:10 +0100

    Added Oppia license to HTTPClientUtils

--- src/org/digitalcampus/oppia/utils/HTTPClientUtils.java
+++ src/org/digitalcampus/oppia/utils/HTTPClientUtils.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of OppiaMobile - https://digital-campus.org/
+ *
+ * OppiaMobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OppiaMobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OppiaMobile. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.digitalcampus.oppia.utils;
 
 import android.content.Context;
@@ -5,8 +22,6 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
 
-import org.apache.http.message.BasicHeader;
-import org.apache.http.params.CoreProtocolPNames;
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.activity.PrefsActivity;
 import org.digitalcampus.oppia.application.MobileLearning;
