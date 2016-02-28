8e49d070df776fd8de6f65b151f267bb64e89a7e
Joseba <jjoseba@gmail.com>
2016-02-28 21:58:43 +0100

    Updated the Device Admin registration task to use also the OkHttp library. Added the final url for the service

--- src/org/digitalcampus/oppia/service/GCMRegistrationService.java
+++ src/org/digitalcampus/oppia/service/GCMRegistrationService.java
@@ -1,46 +1,18 @@
 package org.digitalcampus.oppia.service;
 
 import android.app.IntentService;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
 import android.util.Log;
 
-import com.google.android.gms.gcm.GcmPubSub;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
-import com.splunk.mint.Mint;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.activity.PrefsActivity;
-import org.digitalcampus.oppia.application.DatabaseManager;
-import org.digitalcampus.oppia.application.DbHelper;
 import org.digitalcampus.oppia.application.MobileLearning;
-import org.digitalcampus.oppia.task.Payload;
 import org.digitalcampus.oppia.task.RegisterDeviceRemoteAdminTask;
-import org.digitalcampus.oppia.utils.HTTPConnectionUtils;
-import org.digitalcampus.oppia.utils.MetaDataUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GCMRegistrationService extends IntentService {
 
