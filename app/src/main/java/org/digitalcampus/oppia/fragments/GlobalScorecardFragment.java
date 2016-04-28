c86fe22c47e3d595f26eda6872a9ab2fcb92e9bb
Joseba <jjoseba@gmail.com>
2016-04-28 16:32:07 +0200

    #534: Implemented logic, taking into account the AdminSecurityManager too

--- app/src/main/java/org/digitalcampus/oppia/fragments/GlobalScorecardFragment.java
+++ app/src/main/java/org/digitalcampus/oppia/fragments/GlobalScorecardFragment.java
@@ -24,12 +24,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.activity.CourseIndexActivity;
+import org.digitalcampus.oppia.activity.TagSelectActivity;
 import org.digitalcampus.oppia.adapter.ScorecardListAdapter;
+import org.digitalcampus.oppia.application.AdminSecurityManager;
 import org.digitalcampus.oppia.application.DbHelper;
 import org.digitalcampus.oppia.application.SessionManager;
 import org.digitalcampus.oppia.model.Course;
@@ -66,10 +69,36 @@ public class GlobalScorecardFragment extends Fragment implements AdapterView.OnI
         DbHelper db = DbHelper.getInstance(super.getActivity());
         long userId = db.getUserId(SessionManager.getUsername(getActivity()));
         ArrayList<Course> courses = db.getCourses(userId);
-        scorecardListAdapter = new ScorecardListAdapter(super.getActivity(), courses);
+
         GridView scorecardList = (GridView) super.getActivity().findViewById(R.id.scorecards_list);
-        scorecardList.setAdapter(scorecardListAdapter);
-        scorecardList.setOnItemClickListener(this);
+        View emptyState = this.getActivity().findViewById(R.id.empty_state);
+
+        if (courses.size() == 0){
+            //If there are now courses, display the empty state
+            scorecardList.setVisibility(View.GONE);
+            emptyState.setVisibility(View.VISIBLE);
+
+            Button download = (Button) emptyState.findViewById(R.id.btn_download_courses);
+            download.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AdminSecurityManager.checkAdminPermission(getActivity(), R.id.menu_download, new AdminSecurityManager.AuthListener() {
+                        public void onPermissionGranted() {
+                            startActivity(new Intent(getActivity(), TagSelectActivity.class));
+                        }
+                    });
+                }
+            });
+        }
+        else{
+            scorecardList.setVisibility(View.VISIBLE);
+            emptyState.setVisibility(View.GONE);
+
+            scorecardListAdapter = new ScorecardListAdapter(super.getActivity(), courses);
+            scorecardList.setAdapter(scorecardListAdapter);
+            scorecardList.setOnItemClickListener(this);
+        }
+
     }
 
     @Override
