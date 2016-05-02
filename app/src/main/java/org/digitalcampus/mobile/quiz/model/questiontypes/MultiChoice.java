1f6164c08f606d6e5da2c6588acc476a8523f260
Joseba <jjoseba@gmail.com>
2016-05-02 13:34:15 +0200

    Added Oppia license to all the files where it was missing

--- app/src/main/java/org/digitalcampus/mobile/quiz/model/questiontypes/MultiChoice.java
+++ app/src/main/java/org/digitalcampus/mobile/quiz/model/questiontypes/MultiChoice.java
@@ -1,15 +1,31 @@
-package org.digitalcampus.mobile.quiz.model.questiontypes;
+/*
+ * This file is part of OppiaMobile - https://digital-campus.org/
+ *
+ * OppiaMobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OppiaMobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OppiaMobile. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+package org.digitalcampus.mobile.quiz.model.questiontypes;
 
 import org.digitalcampus.mobile.quiz.model.QuizQuestion;
 import org.digitalcampus.mobile.quiz.model.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MultiChoice implements Serializable, QuizQuestion {
 
