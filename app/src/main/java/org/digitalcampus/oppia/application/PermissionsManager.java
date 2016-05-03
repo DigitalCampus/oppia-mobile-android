7b534b9d6f74fbc6c808ac6a75dde3a5b3010666
Joseba <jjoseba@gmail.com>
2016-05-03 22:09:07 +0200

    #531: Updated to correctly manage the situation where the users checks the "don't ask again" option

--- app/src/main/java/org/digitalcampus/oppia/application/PermissionsManager.java
+++ app/src/main/java/org/digitalcampus/oppia/application/PermissionsManager.java
@@ -2,8 +2,10 @@ package org.digitalcampus.oppia.application;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ import java.util.List;
 
 public class PermissionsManager {
 
+    public final static String TAG = PermissionsManager.class.getSimpleName();
     private static final int PERMISSIONS_REQUEST = 1246;
     private static final List<String> PERMISSIONS_REQUIRED = Arrays.asList(
         //Remember to update this when the Manifest permisssions change!
@@ -20,28 +23,41 @@ public class PermissionsManager {
         Manifest.permission.WRITE_EXTERNAL_STORAGE
     );
 
+    private static boolean isFirstTimeAsked(SharedPreferences prefs, String permission){
+        boolean asked = prefs.getBoolean(permission + "_asked", false);
+        return !asked;
+    }
+
     public static boolean CheckPermissionsAndInform(Activity act){
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             //If sdk version prior to 23 (Android M), the permissions are granted by manifest
             return true;
         }
 
-        int permissionsDenied = 0;
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(act.getApplicationContext());
+        int permissionsAskable = 0;
         List<String> permissionsToAsk = new ArrayList<>();
 
         for (String permission : PERMISSIONS_REQUIRED){
             int permitted = act.checkSelfPermission( permission );
             if( permitted != PackageManager.PERMISSION_GRANTED ) {
-                permissionsDenied++;
+                permissionsToAsk.add(permission);
                 if (act.shouldShowRequestPermissionRationale(permission)){
-                    permissionsToAsk.add(permission);
+                    //The permission has been asked before, and the user answered "no"
+                    permissionsAskable++;
+                }
+                else{
+                    //If is the first time we ask for the permission, we can ask
+                    if (isFirstTimeAsked(prefs, permission))
+                        permissionsAskable++;
+                    //else, the user has answered "no" and checked "Don't ask again"
                 }
             }
         }
 
-        if (permissionsDenied > 0){
-            if (permissionsToAsk.size() == permissionsDenied){
-                //The user has not selected the "Don't show again" option for any permission yet
+        if (permissionsToAsk.size() > 0){
+            if (permissionsToAsk.size() == permissionsAskable){
+                //The user has not selected the "Don't ask again" option for any permission yet
                 act.requestPermissions( permissionsToAsk.toArray( new String[permissionsToAsk.size()] ), PERMISSIONS_REQUEST );
             }
             else{
@@ -50,7 +66,7 @@ public class PermissionsManager {
             }
         }
 
-        return (permissionsDenied > 0);
+        return (permissionsToAsk.size() == 0);
 
     }
 
