da501106f642a2bb92d4bf7f99360d46bfac09e3
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-06-27 18:23:41 +0200

    Added more test methods

--- app/src/androidTest/java/RegisterUITest.java
+++ app/src/androidTest/java/RegisterUITest.java
@@ -1,10 +1,15 @@
+import android.app.Fragment;
+import android.content.res.Resources;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.content.res.ResourcesCompat;
 
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.activity.WelcomeActivity;
+import org.digitalcampus.oppia.application.MobileLearning;
 import org.digitalcampus.oppia.fragments.RegisterFragment;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,7 +38,7 @@ public class RegisterUITest {
             new ActivityTestRule<WelcomeActivity>(WelcomeActivity.class);
 
     @Test
-    public void clickRegisterButton_emptyUsername() throws  Exception {
+    public void clickRegisterButton_UsernameEmpty() throws  Exception {
 
         onView(withId(R.id.welcome_register))
                 .perform(click());
@@ -49,7 +54,23 @@ public class RegisterUITest {
     }
 
     @Test
-    public void clickRegisterButton_emptyEmail() throws  Exception {
+    public void clickRegisterButton_UsernameWithSpaces() throws  Exception {
+
+        onView(withId(R.id.welcome_register))
+                .perform(click());
+
+        onView(withId(R.id.register_form_username_field))
+                .perform(typeText("Username With Spaces"), closeSoftKeyboard());
+
+        onView(withId(R.id.register_btn))
+                .perform(scrollTo(), click());
+
+        onView(withText(R.string.error_register_username_spaces))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickRegisterButton_EmailEmpty() throws  Exception {
 
         onView(withId(R.id.welcome_register))
                 .perform(click());
@@ -64,6 +85,29 @@ public class RegisterUITest {
                 .perform(scrollTo(), click());
 
         onView(withText(R.string.error_register_no_email))
+                .check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void clickRegisterButton_PasswordTooShort() throws  Exception {
+
+        onView(withId(R.id.welcome_register))
+                .perform(click());
+
+        onView(withId(R.id.register_form_username_field))
+                .perform(typeText("Username"), closeSoftKeyboard());
+
+        onView(withId(R.id.register_form_email_field))
+                .perform(typeText("Email"), closeSoftKeyboard());
+
+        onView(withId(R.id.register_form_password_field))
+                .perform(typeText("123"), closeSoftKeyboard());
+
+        onView(withId(R.id.register_btn))
+                .perform(scrollTo(), click());
+
+        onView(withText(String.format(InstrumentationRegistry.getTargetContext().getString(R.string.error_register_password),  MobileLearning.PASSWORD_MIN_LENGTH )))
                 .check(matches(isDisplayed()));
     }
 
