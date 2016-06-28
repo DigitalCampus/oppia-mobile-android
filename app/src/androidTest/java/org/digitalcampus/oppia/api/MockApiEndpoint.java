2ea22c1deb02b28dd5855055b83a6f397ef5f17d
Joseba <jjoseba@gmail.com>
2016-06-28 16:53:37 +0200

    Moved MockApiEndpoint to test submodule

--- app/src/androidTest/java/org/digitalcampus/oppia/api/MockApiEndpoint.java
+++ app/src/androidTest/java/org/digitalcampus/oppia/api/MockApiEndpoint.java
@@ -1,17 +1,8 @@
 package org.digitalcampus.oppia.api;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-
-import org.digitalcampus.mobile.learning.R;
-import org.digitalcampus.oppia.activity.PrefsActivity;
-
 import okhttp3.mockwebserver.MockWebServer;
 
-/**
- * Created by Alberto on 28/06/2016.
- */
 public class MockApiEndpoint implements ApiEndpoint {
     private MockWebServer mockServer;
 
