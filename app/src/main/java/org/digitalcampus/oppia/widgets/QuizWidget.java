c612ce6b0589be22de605e1a8bfcd027e84fcd5d
Joseba <jjoseba@gmail.com>
2016-07-13 19:44:48 +0200

    #574: Check quiz availability in the feedback step to prevent restarting the quiz more than max attempts

--- app/src/main/java/org/digitalcampus/oppia/widgets/QuizWidget.java
+++ app/src/main/java/org/digitalcampus/oppia/widgets/QuizWidget.java
@@ -480,8 +480,18 @@ public class QuizWidget extends WidgetFactory {
 		
 		// Show restart or continue button
 		Button restartBtn = (Button) getView().findViewById(R.id.quiz_results_button);
-		
-		if (this.isBaseline || this.getActivityCompleted()) {
+
+
+        int quizAvailability = checkQuizAvailability();
+        boolean quizAvailable = quizAvailability == QUIZ_AVAILABLE;
+
+        if (!quizAvailable){
+            TextView availabilityMsg = (TextView) getView().findViewById(R.id.quiz_availability_message);
+            availabilityMsg.setText(quizAvailability);
+            availabilityMsg.setVisibility(View.VISIBLE);
+        }
+
+		if (this.isBaseline || this.getActivityCompleted() || !quizAvailable) {
 			restartBtn.setText(super.getActivity().getString(R.string.widget_quiz_baseline_goto_course));
 			restartBtn.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
