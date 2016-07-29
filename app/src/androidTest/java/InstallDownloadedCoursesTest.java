6270823d81498cc0b85fc975db6ae1d7ed8c82f6
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2016-07-29 16:24:06 +0200

    Added Incorrect course test

--- app/src/androidTest/java/InstallDownloadedCoursesTest.java
+++ app/src/androidTest/java/InstallDownloadedCoursesTest.java
@@ -9,12 +9,14 @@ import org.digitalcampus.oppia.listener.SubmitListener;
 import org.digitalcampus.oppia.model.DownloadProgress;
 import org.digitalcampus.oppia.task.InstallDownloadedCoursesTask;
 import org.digitalcampus.oppia.task.Payload;
+import org.digitalcampus.oppia.utils.storage.Storage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.concurrent.CountDownLatch;
@@ -29,6 +31,8 @@ import static junit.framework.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class InstallDownloadedCoursesTest {
     private final String CORRECT_COURSE = "Wash.zip";
+    private final String ALREADY_INSTALLED_COURSE = "";
+    private final String INCORRECT_COURSE = "";
 
     private Context context;
     private CountDownLatch signal;
@@ -47,10 +51,59 @@ public class InstallDownloadedCoursesTest {
 
     @Test
     public void installCourse_correctCourse()throws Exception{
-        FileUtils.copyZipFromAssets(CORRECT_COURSE);
+        String filename = CORRECT_COURSE;
+
+        FileUtils.copyZipFromAssets(filename);  //Copy course zip from assets to download path
 
         String title = CourseUtils.getCourseTitle(context);
 
+        runInstallCourseTask();//Run test task
+
+        signal.await();
+
+        assertTrue(response.isResult());
+        assertEquals(response.getResultResponse(), context.getString(R.string.install_course_complete, title));
+        File outputFile = new File(Storage.getDownloadPath(InstrumentationRegistry.getTargetContext()), filename);
+        assertFalse(outputFile.exists());
+    }
+
+    @Test
+    public void installCourse_existingCourse()throws Exception{
+        String filename = CORRECT_COURSE;
+
+        String title = "";
+        for(int i = 0; i < 2; i++){
+            FileUtils.copyZipFromAssets(filename); //Copy course zip from assets to download path
+            title = CourseUtils.getCourseTitle(context);
+            runInstallCourseTask(); //Run test task
+        }
+
+        signal.await();
+
+        assertFalse(response.isResult());
+        assertEquals(response.getResultResponse(), context.getString(R.string.error_latest_already_installed, title));
+        File outputFile = new File(Storage.getDownloadPath(InstrumentationRegistry.getTargetContext()), filename);
+        assertFalse(outputFile.exists());
+    }
+
+    @Test
+    public void installCourse_errorInstallingCourse()throws Exception{
+        String filename = INCORRECT_COURSE;
+
+        FileUtils.copyZipFromAssets(filename);  //Copy course zip from assets to download path
+
+        String title = CourseUtils.getCourseTitle(context);
+
+        runInstallCourseTask();     //Run test task
+        signal.await();
+
+        assertFalse(response.isResult());
+        assertEquals(response.getResultResponse(), context.getString(R.string.error_installing_course, title));
+        File outputFile = new File(Storage.getDownloadPath(InstrumentationRegistry.getTargetContext()), filename);
+        assertFalse(outputFile.exists());
+    }
+
+    private void runInstallCourseTask(){
         ArrayList<Object> data = new ArrayList<>();
         Payload payload = new Payload(data);
         InstallDownloadedCoursesTask imTask = new InstallDownloadedCoursesTask(context);
@@ -71,49 +124,6 @@ public class InstallDownloadedCoursesTest {
             public void installProgressUpdate(DownloadProgress dp) {  }
         });
         imTask.execute(payload);
-
-        signal.await();
-
-        assertTrue(response.isResult());
-        assertEquals(response.getResultResponse(), context.getString(R.string.install_course_complete, title));
-
-    }
-
-    @Test
-    public void installCourse_existingCourse()throws Exception{
-
-        String title = "";
-        for(int i = 0; i < 2; i++){
-            FileUtils.copyZipFromAssets(CORRECT_COURSE);
-
-            title = CourseUtils.getCourseTitle(context);
-
-            ArrayList<Object> data = new ArrayList<>();
-            Payload payload = new Payload(data);
-            InstallDownloadedCoursesTask imTask = new InstallDownloadedCoursesTask(context);
-            imTask.setInstallerListener(new InstallCourseListener() {
-                @Override
-                public void downloadComplete(Payload p) {  }
-
-                @Override
-                public void downloadProgressUpdate(DownloadProgress dp) {  }
-
-                @Override
-                public void installComplete(Payload r) {
-                    response = r;
-                    signal.countDown();
-                }
-
-                @Override
-                public void installProgressUpdate(DownloadProgress dp) {  }
-            });
-            imTask.execute(payload);
-
-            signal.await();
-        }
-
-        assertFalse(response.isResult());
-        assertEquals(response.getResultResponse(), context.getString(R.string.error_latest_already_installed, title));
     }
 
 }
