3c31785138b4bcf0c18b74b381d3aa25e0da427c
Joseba <jjoseba@gmail.com>
2016-09-12 13:56:56 +0200

    Added Oppia license to files where it was missing

--- app/src/main/java/org/digitalcampus/oppia/model/MultiLangInfo.java
+++ app/src/main/java/org/digitalcampus/oppia/model/MultiLangInfo.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of OppiaMobile - https://digital-campus.org/
+ *
+ * OppiaMobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OppiaMobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OppiaMobile. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.digitalcampus.oppia.model;
 
 import org.json.JSONArray;
@@ -23,7 +40,6 @@ public class MultiLangInfo implements Serializable {
     public void setTitles(ArrayList<Lang> titles) {
         this.titles = titles;
     }
-
 
     public void setTitlesFromJSONString(String jsonStr) {
         setInfoFromJSONString(jsonStr, this.titles, false);
@@ -53,20 +69,17 @@ public class MultiLangInfo implements Serializable {
     public ArrayList<Lang> getLangs() {
         return langs;
     }
+    public void setLangs(ArrayList<Lang> langs) {
+        this.langs = langs;
+    }
 
     public String getLangsJSONString(){
         return getInfoJSONString(this.langs);
     }
 
-    public void setLangs(ArrayList<Lang> langs) {
-        this.langs = langs;
-    }
-
     public void setLangsFromJSONString(String jsonStr) {
         setInfoFromJSONString(jsonStr, this.langs, true);
     }
-
-
 
     private String getInfo(String lang, ArrayList<Lang> values){
         for(Lang l: values){
