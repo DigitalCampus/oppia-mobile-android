291d7b3b98a7bd4cf5f48441a63f8db82ed9878e
Joseba S <jjoseba@gmail.com>
2016-09-20 11:09:33 +0200

    Bugfix from last merge (48f9363a062ba888e6b80fe41e6760deae94e200)

--- app/src/main/java/org/digitalcampus/oppia/fragments/LoginFragment.java
+++ app/src/main/java/org/digitalcampus/oppia/fragments/LoginFragment.java
@@ -60,7 +60,7 @@ public class LoginFragment extends AppFragment implements SubmitListener {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View vv = super.getLayoutInflater(savedInstanceState).inflate(R.layout.fragment_login, container);
+		View vv = super.getLayoutInflater(savedInstanceState).inflate(R.layout.fragment_login, null);
 		LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 		vv.setLayoutParams(lp);
 		return vv;
