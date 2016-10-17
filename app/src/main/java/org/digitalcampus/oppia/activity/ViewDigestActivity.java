910278491b5e9afa8e27dfa650cc3bc8a8b47a8a
Joseba S <jjoseba@gmail.com>
2016-10-17 13:41:12 +0200

    #524: Added intent filter to catch OppiaMobile digest URIs

---
+++ app/src/main/java/org/digitalcampus/oppia/activity/ViewDigestActivity.java
@@ -0,0 +1,15 @@
+package org.digitalcampus.oppia.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.digitalcampus.mobile.learning.R;
+
+public class ViewDigestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_digest);
+    }
+}
