cd664877ee3a65bc9c64ec10b5759056bb0ca8bc
Joseba S <jjoseba@gmail.com>
2016-10-18 12:58:17 +0200

    #524: Improved layout and show activity info

--- app/src/main/java/org/digitalcampus/oppia/activity/ViewDigestActivity.java
+++ app/src/main/java/org/digitalcampus/oppia/activity/ViewDigestActivity.java
@@ -1,17 +1,26 @@
 package org.digitalcampus.oppia.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.application.DbHelper;
 import org.digitalcampus.oppia.application.SessionManager;
 import org.digitalcampus.oppia.model.Activity;
+import org.digitalcampus.oppia.model.Course;
 import org.w3c.dom.Text;
+
+import java.io.File;
+import java.util.Locale;
 
 public class ViewDigestActivity extends AppCompatActivity {
 
@@ -70,7 +79,24 @@ public class ViewDigestActivity extends AppCompatActivity {
     }
 
     private void showActivityDetails(Activity act){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String lang = prefs.getString(PrefsActivity.PREF_LANGUAGE, Locale.getDefault().getLanguage());
 
+        DbHelper db = DbHelper.getInstance(this);
+        long userID = db.getUserId(SessionManager.getUsername(this));
+        Course c = DbHelper.getInstance(this).getCourse(act.getCourseId(), userID);
+
+        TextView title = (TextView) activityDetail.findViewById(R.id.activity_title);
+        TextView courseTitle = (TextView) activityDetail.findViewById(R.id.course_title);
+        ImageView courseImg = (ImageView) activityDetail.findViewById(R.id.course_image);
+        title.setText(act.getTitle(lang));
+        courseTitle.setText(c.getTitle(lang));
+        if(c.getImageFile() != null){
+            String image = c.getImageFileFromRoot();
+            Picasso.with(this).load(new File(image))
+                    .placeholder(R.drawable.default_course)
+                    .into(courseImg);
+        }
     }
 
     @Override
