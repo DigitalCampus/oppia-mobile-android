8db09221be319e1d2cab0e729bc9d1425fe4f610
Alberto Moreta <amoreta@chaotic-kingdoms.com>
2017-02-08 08:46:32 +0100

    Changed method call for cleaning directories

--- app/src/androidTest/java/Utils/CourseUtils.java
+++ app/src/androidTest/java/Utils/CourseUtils.java
@@ -1,15 +1,10 @@
 package Utils;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
 
-import org.digitalcampus.oppia.activity.PrefsActivity;
 import org.digitalcampus.oppia.application.DbHelper;
-import org.digitalcampus.oppia.application.MobileLearning;
 import org.digitalcampus.oppia.application.SessionManager;
-import org.digitalcampus.oppia.exception.InvalidXMLException;
 import org.digitalcampus.oppia.model.Activity;
 import org.digitalcampus.oppia.model.CompleteCourse;
 import org.digitalcampus.oppia.model.Course;
@@ -18,16 +13,10 @@ import org.digitalcampus.oppia.model.MultiLangInfo;
 import org.digitalcampus.oppia.model.Section;
 import org.digitalcampus.oppia.utils.storage.*;
 import org.digitalcampus.oppia.utils.storage.FileUtils;
-import org.digitalcampus.oppia.utils.xmlreaders.CourseXMLReader;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Locale;
 
-import static org.apache.commons.io.FileUtils.cleanDirectory;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -42,37 +31,31 @@ public class CourseUtils {
 
     public static void cleanUp(){
         //Clean course folders and database
-        try {
 
-            Context ctx = InstrumentationRegistry.getTargetContext();
+        Context ctx = InstrumentationRegistry.getTargetContext();
 
-            //Clean downloads folder
-            File downloadsFolder = new File(Storage.getDownloadPath(ctx));
-            if(downloadsFolder.exists()) {
-                cleanDirectory(downloadsFolder);
-            }
+        //Clean downloads folder
+        File downloadsFolder = new File(Storage.getDownloadPath(ctx));
+        if(downloadsFolder.exists()) {
+            FileUtils.cleanDir(downloadsFolder);
+        }
 
-            //Clean courses folder
-            File coursesFolder = new File(Storage.getCoursesPath(ctx));
-            if(coursesFolder.exists()) {
-                cleanDirectory(coursesFolder);
-            }
+        //Clean courses folder
+        File coursesFolder = new File(Storage.getCoursesPath(ctx));
+        if(coursesFolder.exists()) {
+            FileUtils.cleanDir(coursesFolder);
+        }
 
-            //Clean temp folder
-            File tempFolder = new File(Storage.getStorageLocationRoot(ctx) + "temp/");
-            if (tempFolder.exists()) {
-                cleanDirectory(tempFolder);
-            }
+        //Clean temp folder
+        File tempFolder = new File(Storage.getStorageLocationRoot(ctx) + "temp/");
+        if (tempFolder.exists()) {
+            FileUtils.cleanDir(tempFolder);
+        }
 
-            //Clean database
-            DbHelper db = DbHelper.getInstance(ctx);
-            for(Course c : db.getAllCourses()){
-                db.deleteCourse(c.getCourseId());
-            }
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        //Clean database
+        DbHelper db = DbHelper.getInstance(ctx);
+        for(Course c : db.getAllCourses()){
+            db.deleteCourse(c.getCourseId());
         }
     }
 
