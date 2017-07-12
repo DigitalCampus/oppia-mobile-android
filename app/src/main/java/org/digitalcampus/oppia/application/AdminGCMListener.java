3b21800b14f9a0641c1538de6d4ab7b4e8769379
Alberto Moreta <alberto@digital-campus.org>
2017-07-12 11:21:08 +0200

    #622: Added option to disable notifications

--- app/src/main/java/org/digitalcampus/oppia/application/AdminGCMListener.java
+++ app/src/main/java/org/digitalcampus/oppia/application/AdminGCMListener.java
@@ -36,7 +36,7 @@ import org.digitalcampus.mobile.learning.BuildConfig;
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.activity.PrefsActivity;
 import org.digitalcampus.oppia.activity.StartUpActivity;
-import org.digitalcampus.oppia.utils.ui.OppiaNotificationBuilder;
+import org.digitalcampus.oppia.utils.ui.OppiaNotificationUtils;
 
 @SuppressLint("NewApi")
 public class AdminGCMListener extends GcmListenerService {
@@ -108,16 +108,14 @@ public class AdminGCMListener extends GcmListenerService {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        NotificationCompat.Builder notifBuilder = OppiaNotificationBuilder.getBaseBuilder(this, true);
+        NotificationCompat.Builder notifBuilder = OppiaNotificationUtils.getBaseBuilder(this, true);
         notifBuilder
-            .setContentTitle(getString(R.string.notification_remote_admin_title))
-            .setContentText(message)
-            .setContentIntent(pendingIntent)
-            .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
-            .build();
+                .setContentTitle(getString(R.string.notification_remote_admin_title))
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                .build();
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTIFICATION_ID, notifBuilder.build());
+        OppiaNotificationUtils.sendNotification(this, NOTIFICATION_ID, notifBuilder.build());
     }
 }
