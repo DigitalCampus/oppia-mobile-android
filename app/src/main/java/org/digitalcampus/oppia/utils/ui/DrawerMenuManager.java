570e81a00ac74677f85075d395151666fc1dc945
Joseba S <jjoseba@gmail.com>
2018-08-08 09:43:17 +0200

    Make "Download courses" menu option's visibility configurable via settings

--- app/src/main/java/org/digitalcampus/oppia/utils/ui/DrawerMenuManager.java
+++ app/src/main/java/org/digitalcampus/oppia/utils/ui/DrawerMenuManager.java
@@ -125,8 +125,8 @@ public class DrawerMenuManager {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(drawerAct);
         itemLogout.setVisible(prefs.getBoolean(PrefsActivity.PREF_LOGOUT_ENABLED, MobileLearning.MENU_ALLOW_LOGOUT));
         itemSettings.setVisible(MobileLearning.MENU_ALLOW_SETTINGS);
-        itemCourseDownload.setVisible(MobileLearning.MENU_ALLOW_COURSE_DOWNLOAD);
         itemLanguageDialog.setVisible(customOptions.containsKey(R.id.menu_language));
+        itemCourseDownload.setVisible(prefs.getBoolean(PrefsActivity.PREF_DOWNLOAD_ENABLED, MobileLearning.MENU_ALLOW_COURSE_DOWNLOAD));
         itemSync.setVisible(MobileLearning.MENU_ALLOW_SYNC);
     }
 
