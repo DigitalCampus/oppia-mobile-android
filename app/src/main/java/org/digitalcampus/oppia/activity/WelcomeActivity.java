ede7f52bf992d306fdc1d7670c6fd3cd212afd34
Joseba S <jjoseba@gmail.com>
2018-11-20 18:07:06 +0100

    #754: Removed tabs from welcome screen

--- app/src/main/java/org/digitalcampus/oppia/activity/WelcomeActivity.java
+++ app/src/main/java/org/digitalcampus/oppia/activity/WelcomeActivity.java
@@ -24,7 +24,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.support.v7.widget.Toolbar;
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.adapter.ActivityPagerAdapter;
 import org.digitalcampus.oppia.fragments.LoginFragment;
@@ -39,24 +39,36 @@ import java.util.List;
 public class WelcomeActivity extends AppActivity {
 
 	public static final String TAG = WelcomeActivity.class.getSimpleName();
-	private ViewPager viewPager;
+	public static final int TAB_WELCOME = 0;
+    public static final int TAB_LOGIN = 1;
+    public static final int TAB_REGISTER = 2;
+    public static final int TAB_PASSWORD = 3;
+
+    private ViewPager viewPager;
     private TabLayout tabs;
-    private int currentTab = 0;
+    private int currentTab = TAB_WELCOME;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.activity_about);
-
-        tabs = (TabLayout) findViewById(R.id.tabs_toolbar);
-		viewPager = (ViewPager) findViewById(R.id.activity_about_pager);
-
+		setContentView(R.layout.activity_welcome);
+        //tabs = (TabLayout) findViewById(R.id.tabs_toolbar);
+		viewPager = (ViewPager) findViewById(R.id.activity_welcome_pager);
 	}
 	
 	@Override
 	public void onStart() {
-		super.onStart();
+		super.onStart(true, false);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		toolbar.getMenu().clear();
+		toolbar.inflateMenu(R.menu.activity_welcome);
+		toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				return onOptionsItemSelected(item);
+			}
+		});
+
 		List<Fragment> fragments = new ArrayList<>();
         List<String> tabTitles = new ArrayList<>();
 		
@@ -78,9 +90,9 @@ public class WelcomeActivity extends AppActivity {
 
         ActivityPagerAdapter apAdapter = new ActivityPagerAdapter(this, getSupportFragmentManager(), fragments, tabTitles);
 		viewPager.setAdapter(apAdapter);
-        tabs.setupWithViewPager(viewPager);
-        tabs.setTabMode(TabLayout.MODE_FIXED);
-        tabs.setTabGravity(TabLayout.GRAVITY_FILL);
+        //tabs.setupWithViewPager(viewPager);
+        //tabs.setTabMode(TabLayout.MODE_FIXED);
+        //tabs.setTabGravity(TabLayout.GRAVITY_FILL);
 
 		viewPager.setCurrentItem(currentTab);
         viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
---
+++ app/src/main/java/org/digitalcampus/oppia/utils/ui/NoSwipeableViewpager.java
@@ -0,0 +1,32 @@
+package org.digitalcampus.oppia.utils.ui;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NoSwipeableViewpager extends ViewPager {
+
+    private boolean canScroll;
+
+    public NoSwipeableViewpager(Context context) {
+        super(context);
+        canScroll = false;
+    }
+    public NoSwipeableViewpager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        canScroll = false;
+    }
+    public void setCanScroll(boolean canScroll) {
+        this.canScroll = canScroll;
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return canScroll && super.onTouchEvent(ev);
+    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return canScroll && super.onInterceptTouchEvent(ev);
+    }
+
+}
