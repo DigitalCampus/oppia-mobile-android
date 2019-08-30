65514369726026876370a9aee162fd198bff4dc0
julio <jberzal86@gmail.com>
2019-08-30 13:06:02 +0200

    - started migration. dependencies versions updated

--- app/src/androidTest/java/UI/LoginUITest.java
+++ app/src/androidTest/java/UI/LoginUITest.java
@@ -10,8 +10,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import TestRules.DisableAnimationsRule;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -63,7 +61,7 @@ public class LoginUITest {
     }
 
     @Test
-    public void changeActivityWhenTheCredentialsAreCorrect() throws Exception{
+    public void changeActivityWhenTheCredentialsAreCorrect() throws Exception {
 
        onView(withId(R.id.welcome_login))
                .perform(scrollTo(), click());
--- app/src/main/java/org/digitalcampus/oppia/service/TrackerService.java
+++ app/src/main/java/org/digitalcampus/oppia/service/TrackerService.java
@@ -19,12 +19,9 @@ package org.digitalcampus.oppia.service;
 
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -41,11 +38,9 @@ import org.digitalcampus.oppia.application.MobileLearning;
 import org.digitalcampus.oppia.application.SessionManager;
 import org.digitalcampus.oppia.listener.APIRequestListener;
 import org.digitalcampus.oppia.model.QuizAttempt;
-import org.digitalcampus.oppia.model.User;
 import org.digitalcampus.oppia.task.APIUserRequestTask;
 import org.digitalcampus.oppia.task.FetchServerInfoTask;
 import org.digitalcampus.oppia.task.Payload;
-import org.digitalcampus.oppia.task.RegisterTask;
 import org.digitalcampus.oppia.task.SubmitQuizAttemptsTask;
 import org.digitalcampus.oppia.task.SubmitTrackerMultipleTask;
 import org.digitalcampus.oppia.utils.ConnectionUtils;
@@ -152,6 +147,7 @@ public class TrackerService extends Service implements APIRequestListener {
 				app.omSubmitQuizAttemptsTask.execute(p);
 			}
 		}
+
 	}
 
 	public void apiRequestComplete(Payload response) {
