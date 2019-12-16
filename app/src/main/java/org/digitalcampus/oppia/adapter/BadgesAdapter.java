30bef9f76adc504c6e163f5355fe6284ca89ec1f
julio <jberzal86@gmail.com>
2019-12-16 09:10:49 +0100

    - Changed RecyclerView in Badges screen

---
+++ app/src/main/java/org/digitalcampus/oppia/adapter/BadgesAdapter.java
@@ -0,0 +1,98 @@
+package org.digitalcampus.oppia.adapter;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.digitalcampus.mobile.learning.R;
+import org.digitalcampus.oppia.model.Badges;
+
+import java.util.List;
+
+public class BadgesAdapter extends RecyclerView.Adapter<BadgesAdapter.ViewHolder> {
+
+
+    private List<Badges> badges;
+    private Context context;
+    private OnItemClickListener itemClickListener;
+
+
+    public BadgesAdapter(Context context, List<Badges> badges) {
+        this.context = context;
+        this.badges = badges;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View contactView = LayoutInflater.from(context).inflate(R.layout.fragment_badges_list_row, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, final int position) {
+
+        final Badges b = getItemAtPosition(position);
+
+        viewHolder.badgeDescription.setText(b.getDescription());
+        viewHolder.badgeDate.setText(b.getDateAsString());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return badges.size();
+    }
+
+    public Badges getItemAtPosition(int position) {
+        return badges.get(position);
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public View rootView;
+        private TextView badgeDescription;
+        private TextView badgeDate;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+
+            badgeDescription = itemView.findViewById(R.id.badges_description);
+            badgeDate = itemView.findViewById(R.id.badges_date);
+
+            rootView = itemView;
+
+//            rootView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (itemClickListener != null) {
+//                        itemClickListener.onItemClick(v, getAdapterPosition());
+//                    }
+//                }
+//            });
+        }
+
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.itemClickListener = listener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
+
+
