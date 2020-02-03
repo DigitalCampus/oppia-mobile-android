c2e5d916c53b130defa077c845c09fea9d302141
Joseba S <jjoseba@gmail.com>
2020-02-03 11:06:20 +0100

    #975: Correctly close connection when pressing toolbar back button

--- app/src/main/java/org/digitalcampus/oppia/activity/SyncActivity.java
+++ app/src/main/java/org/digitalcampus/oppia/activity/SyncActivity.java
@@ -406,9 +406,7 @@ public class SyncActivity extends AppActivity implements InstallCourseListener, 
         if (isBluetoothAvailable()){
             bluetoothManager.disconnect(false);
         }
-        else{
-            super.onBackPressed();
-        }
+        super.onBackPressed();
 
     }
 
@@ -670,8 +668,11 @@ public class SyncActivity extends AppActivity implements InstallCourseListener, 
             case R.id.menu_disconnect:
                 bluetoothManager.disconnect(true);
                 break;
-        }
 
+            case android.R.id.home:
+                this.onBackPressed();
+                break;
+        }
 
         return super.onOptionsItemSelected(item);
     }
