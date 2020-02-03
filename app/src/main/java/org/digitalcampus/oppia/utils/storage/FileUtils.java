DigitalCampus/oppia-mobile-android

commit 829eb9c212dc07a63e187a0f2978886df058e598

- most sonarqube bugfixes recomendations done

app/src/main/java/org/digitalcampus/oppia/utils/storage/FileUtils.java
@@ -1,16 +1,16 @@
-/* 
+/*
  * This file is part of OppiaMobile - https://digital-campus.org/
- * 
+ *
  * OppiaMobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * OppiaMobile is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with OppiaMobile. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -51,6 +51,9 @@ public class FileUtils {
 		FileInputStream fis = null;
 		ZipInputStream zis = null;
 
+		FileOutputStream fos = null;
+		BufferedOutputStream dest = null;
+
 		try {
 			// first make sure that all the arguments are valid and not null
 			if (srcDirectory == null) {
@@ -87,7 +90,6 @@ public class FileUtils {
 			}
 
 			// now start with unzip process
-			BufferedOutputStream dest;
 			fis = new FileInputStream(sourceFile);
 			zis = new ZipInputStream(new BufferedInputStream(fis));
 			ZipEntry entry;
@@ -105,7 +107,7 @@ public class FileUtils {
 
 				// write the file to the disk
 				if (!f.isDirectory()) {
-					FileOutputStream fos = new FileOutputStream(f);
+					fos = new FileOutputStream(f);
 					dest = new BufferedOutputStream(fos, BUFFER_SIZE);
 
 					// this counter is a hack to prevent getting stuck when
@@ -132,11 +134,10 @@ public class FileUtils {
 			Log.d(TAG, "Exception:", e);
 			return false;
 		} finally {
-			try {
-				fis.close();
-				zis.close();
-			} catch (Exception e) {
-			}
+			try { fis.close(); } catch (Exception e) { }
+			try { zis.close(); } catch (Exception e) { }
+            try { fos.close(); } catch (Exception e) { }
+            try { dest.close(); } catch (Exception e) { }
 		}
 
 		return true;
@@ -145,15 +146,20 @@ public class FileUtils {
 	public static boolean zipFileAtPath(File sourceFile, File zipDestination) {
 		final int BUFFER = 2048;
 		Log.d(TAG, "Zipping " + sourceFile + " into " + zipDestination);
+
+		FileOutputStream dest = null;
+		ZipOutputStream out = null;
+		FileInputStream fi = null;
+		BufferedInputStream origin = null;
+
 		try {
-			BufferedInputStream origin = null;
-			FileOutputStream dest = new FileOutputStream(zipDestination);
-			ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
+			dest = new FileOutputStream(zipDestination);
+			out = new ZipOutputStream(new BufferedOutputStream(dest));
 			if (sourceFile.isDirectory()) {
 				zipSubFolder(out, sourceFile, sourceFile.getParent().length());
 			} else {
 				byte data[] = new byte[BUFFER];
-				FileInputStream fi = new FileInputStream(zipDestination);
+				fi = new FileInputStream(zipDestination);
 				origin = new BufferedInputStream(fi, BUFFER);
 				ZipEntry entry = new ZipEntry(getLastPathComponent(zipDestination.getPath()));
 				out.putNextEntry(entry);
@@ -162,42 +168,60 @@ public class FileUtils {
 					out.write(data, 0, count);
 				}
 			}
-			out.close();
+
 		} catch (Exception e) {
 			Mint.logException(e);
 			Log.d(TAG, "Exception:", e);
 			return false;
+		} finally {
+			try { dest.close(); } catch (Exception e) { }
+			try { out.close(); } catch (Exception e) { }
+			try { fi.close(); } catch (Exception e) { }
+			try { origin.close(); } catch (Exception e) { }
 		}
 		return true;
 	}
 
 
-	private static void zipSubFolder(ZipOutputStream out, File folder,
-							  int basePathLength) throws IOException {
+	private static void zipSubFolder(ZipOutputStream out, File folder, int basePathLength) {
 
 		final int BUFFER = 2048;
 
 		Log.d(TAG, "Zipping folder " + folder.getPath());
 
 		File[] fileList = folder.listFiles();
-		BufferedInputStream origin = null;
+
 		for (File file : fileList) {
 			if (file.isDirectory()) {
 				zipSubFolder(out, file, basePathLength);
 			} else {
-				byte data[] = new byte[BUFFER];
-				String unmodifiedFilePath = file.getPath();
-				String relativePath = unmodifiedFilePath
-						.substring(basePathLength);
-				FileInputStream fi = new FileInputStream(unmodifiedFilePath);
-				origin = new BufferedInputStream(fi, BUFFER);
-				ZipEntry entry = new ZipEntry(relativePath);
-				out.putNextEntry(entry);
-				int count;
-				while ((count = origin.read(data, 0, BUFFER)) != -1) {
-					out.write(data, 0, count);
+
+				BufferedInputStream origin = null;
+				FileInputStream fi = null;
+
+				try {
+
+					byte data[] = new byte[BUFFER];
+					String unmodifiedFilePath = file.getPath();
+					String relativePath = unmodifiedFilePath
+							.substring(basePathLength);
+					fi = new FileInputStream(unmodifiedFilePath);
+					origin = new BufferedInputStream(fi, BUFFER);
+					ZipEntry entry = new ZipEntry(relativePath);
+					out.putNextEntry(entry);
+					int count;
+					while ((count = origin.read(data, 0, BUFFER)) != -1) {
+						out.write(data, 0, count);
+					}
+
+				} catch (IOException e) {
+					e.printStackTrace();
+
+				} finally {
+					try { fi.close(); } catch (Exception e) { }
+					try { origin.close(); } catch (Exception e) { }
+					try { out.close(); } catch (Exception e) { }
 				}
-				origin.close();
 			}
 		}
 	}
@@ -278,7 +302,10 @@ public class FileUtils {
 
 		// delete zip file from download dir
 		File zip = new File(path);
-		zip.delete();
+		if (!zip.delete()) {
+			Log.e(TAG, "cleanUp: File could not be deleted: " + zip.getAbsolutePath());
+		}
+
 	}
 
 	public static String readFile(String file) throws IOException {

app/build.gradle
@@ -195,6 +195,8 @@ dependencies {
     implementation 'androidx.concurrent:concurrent-futures:1.0.0-rc01'
     androidTestImplementation "androidx.work:work-testing:$work_version"
 
+    implementation 'javax.xml.stream:stax-api:1.0-2'
+
 //    implementation 'androidx.multidex:multidex:2.0.1'
 
     //debugImplementation 'im.dino:dbinspector:3.4.1@aar'

app/src/main/java/org/digitalcampus/oppia/activity/AppActivity.java
@@ -68,7 +68,7 @@ import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class AppActivity extends AppCompatActivity implements APIKeyRequestListener, GamificationEventListener {
 
-    public static final String TAG = AppActivity.class.getSimpleName();
+    protected final String TAG = this.getClass().getSimpleName();
 
     GamificationBroadcastReceiver gamificationReceiver;
     private Menu optionsMenu;
@@ -382,7 +382,6 @@ public class AppActivity extends AppCompatActivity implements APIKeyRequestListe
 
     private void waitAndClose(final Snackbar snackbar) {
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         int durationViewPoints = Integer.parseInt(prefs.getString(PrefsActivity.PREF_DURATION_GAMIFICATION_POINTS_VIEW,
                 String.valueOf(Gamification.DURATION_GAMIFICATION_POINTS_VIEW)));
 

app/src/main/java/org/digitalcampus/oppia/activity/DeviceListActivity.java
@@ -55,7 +55,7 @@ public class DeviceListActivity extends Activity {
 
     private static final String TAG = "DeviceListActivity";
 
-    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    public static final String EXTRA_DEVICE_ADDRESS = "device_address";
     private static final List<String> BLUETOOTH_PERMISSIONS = Arrays.asList(
             //Remember to update this when the Manifest permissions change!
             Manifest.permission.ACCESS_COARSE_LOCATION,

app/src/main/java/org/digitalcampus/oppia/activity/PrefsActivity.java
@@ -55,8 +55,6 @@ import org.digitalcampus.oppia.utils.storage.StorageAccessStrategy;
 import org.digitalcampus.oppia.utils.storage.StorageAccessStrategyFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class PrefsActivity extends AppActivity implements SharedPreferences.OnSharedPreferenceChangeListener, MoveStorageListener {
 	
@@ -91,10 +89,11 @@ public class PrefsActivity extends AppActivity implements SharedPreferences.OnSh
 	public static final String PREF_HIGHLIGHT_COMPLETED = "prefHighlightCompleted";
 	public static final String PREF_TEXT_SIZE = "prefTextSize";
 
-    public static final List<String> USER_STRING_PREFS =  Arrays.asList(
-        PREF_PHONE_NO, PREF_LANGUAGE, PREF_NO_SCHEDULE_REMINDERS, PREF_TEXT_SIZE);
-    public static final List<String> USER_BOOLEAN_PREFS = Arrays.asList(
-        PREF_SHOW_SCHEDULE_REMINDERS,  PREF_SHOW_COURSE_DESC, PREF_SHOW_PROGRESS_BAR, PREF_SHOW_SECTION_NOS, PREF_HIGHLIGHT_COMPLETED );
+    public static final String PREF_DISABLE_NOTIFICATIONS = "prefDisableNotifications";
+    public static final String PREF_SHOW_GAMIFICATION_EVENTS = "prefShowGamificationEvents";
+    public static final String PREF_GAMIFICATION_POINTS_ANIMATION = "prefGamificationPointsAnimation";
+    public static final String PREF_DURATION_GAMIFICATION_POINTS_VIEW = "prefDurationGamificationPointsView";
+
 	/*
 	 * End personal prefs
 	 */
@@ -104,10 +103,6 @@ public class PrefsActivity extends AppActivity implements SharedPreferences.OnSh
     public static final String PREF_CHANGE_LANGUAGE_ENABLED = "prefChangeLanguageEnabled";
 	public static final String PREF_DELETE_COURSE_ENABLED = "prefDeleteCourseEnabled";
 	public static final String PREF_DOWNLOAD_VIA_CELLULAR_ENABLED = "prefDownloadViaCellularEnabled";
-    public static final String PREF_DISABLE_NOTIFICATIONS = "prefDisableNotifications";
-    public static final String PREF_SHOW_GAMIFICATION_EVENTS = "prefShowGamificationEvents";
-    public static final String PREF_GAMIFICATION_POINTS_ANIMATION = "prefGamificationPointsAnimation";
-    public static final String PREF_DURATION_GAMIFICATION_POINTS_VIEW = "prefDurationGamificationPointsView";
 
     public static final String PREF_LAST_LEADERBOARD_FETCH = "prefLastLeaderboardFetch";
 
@@ -117,7 +112,7 @@ public class PrefsActivity extends AppActivity implements SharedPreferences.OnSh
     public static final String STORAGE_NEEDS_PERMISSIONS = "storageNeedsPermissions";
 
     public static final String PREF_ADMIN_PROTECTION = "prefAdminProtection";
-    public static final String PREF_ADMIN_PASSWORD = "prefAdminPassword";
+    public static final String PREF_ADMIN_PASSWORD = "prefAdminPassword"; //NOSONAR
     public static final String LAST_ACTIVE_TIME = "prefLastActiveTime";
 
     //Google Cloud Messaging preferences

app/src/main/java/org/digitalcampus/oppia/adapter/ActivityTypesAdapter.java
@@ -4,12 +4,13 @@ package org.digitalcampus.oppia.adapter;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
-import androidx.appcompat.widget.AppCompatImageButton;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.digitalcampus.mobile.learning.R;
 import org.digitalcampus.oppia.model.ActivityType;
@@ -78,7 +79,7 @@ public class ActivityTypesAdapter extends RecyclerView.Adapter<ActivityTypesAdap
 
         private final TextView tvActivityType;
         private final AppCompatImageButton imgShowHide;
-        public View rootView;
+        private View rootView;
 
         public ViewHolder(View itemView) {
 

app/src/main/java/org/digitalcampus/oppia/adapter/BadgesListAdapter.java
@@ -17,11 +17,6 @@
 
 package org.digitalcampus.oppia.adapter;
 
-import java.util.ArrayList;
-
-import org.digitalcampus.mobile.learning.R;
-import org.digitalcampus.oppia.model.Badges;
-
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -29,6 +24,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import org.digitalcampus.mobile.learning.R;
+import org.digitalcampus.oppia.model.Badges;
+
+import java.util.ArrayList;
 
 public class BadgesListAdapter extends ArrayAdapter<Badges>{
 
@@ -44,8 +44,8 @@ public class BadgesListAdapter extends ArrayAdapter<Badges>{
 	}
 
     static class BadgeViewHolder{
-        TextView badgeDescription;
-        TextView badgeDate;
+        private TextView badgeDescription;
+        private TextView badgeDate;
     }
 
 	@Override

app/src/main/java/org/digitalcampus/oppia/adapter/CourseQuizzesGridAdapter.java
@@ -43,8 +43,8 @@ public class CourseQuizzesGridAdapter  extends ArrayAdapter<QuizStats> {
     }
 
     static class QuizStatsViewHolder{
-        TextView percent;
-        View baseView;
+        private TextView percent;
+        private View baseView;
     }
 
     @Override

commit ef830405a12c31378c62c797cf88876b4dfceb1c

Solve problem with course backup zipping (introduced in 829eb9c212dc07a63e187a0f2978886df058e598)

app/src/main/java/org/digitalcampus/oppia/utils/storage/FileUtils.java
@@ -48,12 +48,6 @@ public class FileUtils {
 	// placed in the destination directory
 	// destination directory should be created first
 	public static boolean unzipFiles(String srcDirectory, String srcFile, String destDirectory) {
-		FileInputStream fis = null;
-		ZipInputStream zis = null;
-
-		FileOutputStream fos = null;
-		BufferedOutputStream dest = null;
-
 		try {
 			// first make sure that all the arguments are valid and not null
 			if (srcDirectory == null) {
@@ -90,8 +84,9 @@ public class FileUtils {
 			}
 
 			// now start with unzip process
-			fis = new FileInputStream(sourceFile);
-			zis = new ZipInputStream(new BufferedInputStream(fis));
+			BufferedOutputStream dest;
+			FileInputStream fis = new FileInputStream(sourceFile);
+			ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
 			ZipEntry entry;
 
 			while ((entry = zis.getNextEntry()) != null) {
@@ -107,7 +102,7 @@ public class FileUtils {
 
 				// write the file to the disk
 				if (!f.isDirectory()) {
-					fos = new FileOutputStream(f);
+					FileOutputStream fos = new FileOutputStream(f);
 					dest = new BufferedOutputStream(fos, BUFFER_SIZE);
 
 					// this counter is a hack to prevent getting stuck when
@@ -129,15 +124,14 @@ public class FileUtils {
 				}
 			}
 
+			// we are done with all the files
+			// close the zip file
+			zis.close();
+
 		} catch (Exception e) {
 			Mint.logException(e);
 			Log.d(TAG, "Exception:", e);
 			return false;
-		} finally {
-			try { if(fis != null) fis.close(); } catch (Exception e) { }
-			try { if(zis != null) zis.close(); } catch (Exception e) { }
-            try { if(fos != null) fos.close(); } catch (Exception e) { }
-            try { if(dest != null) dest.close(); } catch (Exception e) { }
 		}
 
 		return true;
@@ -146,20 +140,15 @@ public class FileUtils {
 	public static boolean zipFileAtPath(File sourceFile, File zipDestination) {
 		final int BUFFER = 2048;
 		Log.d(TAG, "Zipping " + sourceFile + " into " + zipDestination);
-
-		FileOutputStream dest = null;
-		ZipOutputStream out = null;
-		FileInputStream fi = null;
-		BufferedInputStream origin = null;
-
 		try {
-			dest = new FileOutputStream(zipDestination);
-			out = new ZipOutputStream(new BufferedOutputStream(dest));
+			BufferedInputStream origin = null;
+			FileOutputStream dest = new FileOutputStream(zipDestination);
+			ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
 			if (sourceFile.isDirectory()) {
 				zipSubFolder(out, sourceFile, sourceFile.getParent().length());
 			} else {
 				byte data[] = new byte[BUFFER];
-				fi = new FileInputStream(zipDestination);
+				FileInputStream fi = new FileInputStream(zipDestination);
 				origin = new BufferedInputStream(fi, BUFFER);
 				ZipEntry entry = new ZipEntry(getLastPathComponent(zipDestination.getPath()));
 				out.putNextEntry(entry);
@@ -168,62 +157,44 @@ public class FileUtils {
 					out.write(data, 0, count);
 				}
 			}
-
+			out.close();
 		} catch (Exception e) {
 			Mint.logException(e);
 			Log.d(TAG, "Exception:", e);
 			return false;
-		} finally {
-			try { if(dest != null) dest.close(); } catch (Exception e) { }
-			try { if(out != null) out.close(); } catch (Exception e) { }
-			try { if(fi != null) fi.close(); } catch (Exception e) { }
-			try { if(origin != null) origin.close(); } catch (Exception e) { }
 		}
 		return true;
 	}
 
 
-	private static void zipSubFolder(ZipOutputStream out, File folder, int basePathLength) {
+	private static void zipSubFolder(ZipOutputStream out, File folder,
+									 int basePathLength) throws IOException {
 
 		final int BUFFER = 2048;
 
 		Log.d(TAG, "Zipping folder " + folder.getPath());
 
 		File[] fileList = folder.listFiles();
-
+		BufferedInputStream origin = null;
 		for (File file : fileList) {
 			if (file.isDirectory()) {
 				zipSubFolder(out, file, basePathLength);
 			} else {
-
-				BufferedInputStream origin = null;
-				FileInputStream fi = null;
-
-				try {
-
-					byte data[] = new byte[BUFFER];
-					String unmodifiedFilePath = file.getPath();
-					String relativePath = unmodifiedFilePath
-							.substring(basePathLength);
-					fi = new FileInputStream(unmodifiedFilePath);
-					origin = new BufferedInputStream(fi, BUFFER);
-					ZipEntry entry = new ZipEntry(relativePath);
-					out.putNextEntry(entry);
-					int count;
-					while ((count = origin.read(data, 0, BUFFER)) != -1) {
-						out.write(data, 0, count);
-					}
-
-				} catch (IOException e) {
-					Log.e(TAG, "zipSubFolder: ", e);
-
-				} finally {
-					try { if(fi != null) fi.close(); } catch (Exception e) { }
-					try { if(origin != null) origin.close(); } catch (Exception e) { }
+				byte data[] = new byte[BUFFER];
+				String unmodifiedFilePath = file.getPath();
+				String relativePath = unmodifiedFilePath
+						.substring(basePathLength);
+				FileInputStream fi = new FileInputStream(unmodifiedFilePath);
+				origin = new BufferedInputStream(fi, BUFFER);
+				ZipEntry entry = new ZipEntry(relativePath);
+				out.putNextEntry(entry);
+				int count;
+				while ((count = origin.read(data, 0, BUFFER)) != -1) {
+					out.write(data, 0, count);
 				}
+				origin.close();
 			}
 		}
-
 	}
 
 	/*
@@ -253,66 +224,61 @@ public class FileUtils {
 		}
 	}
 
-    public static boolean cleanDir(File dir){
-        if (dir.isDirectory()) {
-            String[] children = dir.list();
-			if (children != null) {
-				for (String dirFiles : children) {
-					File fileToDelete = new File(dir, dirFiles);
-					boolean success = deleteDir(fileToDelete);
-					if (!success) {
-						return false;
-					}
+	public static boolean cleanDir(File dir){
+		if (dir.isDirectory()) {
+			String[] children = dir.list();
+			for (String dirFiles : children) {
+				File fileToDelete = new File(dir, dirFiles);
+				boolean success = deleteDir(fileToDelete);
+				if (!success) {
+					return false;
 				}
 			}
-        }
-        return true;
-    }
+		}
+		return true;
+	}
 
 	// Deletes all files and subdirectories under dir.
 	// Returns true if all deletions were successful.
 	// If a deletion fails, the method stops attempting to delete and returns
 	// false.
 	public static boolean deleteDir(File dir) {
-        if (cleanDir(dir)){
-            // The directory is now empty so delete it
-            return dir.delete();
-        }
-        else {
-            return false;
-        }
+		if (cleanDir(dir)){
+			// The directory is now empty so delete it
+			return dir.delete();
+		}
+		else {
+			return false;
+		}
 	}
 
-    public static long dirSize(File dir){
-        if (dir.exists() && dir.isDirectory()) {
-            long result = 0;
-            File[] fileList = dir.listFiles();
-            for (File file : fileList) {
-                if (file.isDirectory()) {
-                    result += dirSize(file);
-                } else {
-                    result += file.length();
-                }
-            }
-            return result;
-        }
-        return 0;
-    }
+	public static long dirSize(File dir){
+		if (dir.exists() && dir.isDirectory()) {
+			long result = 0;
+			File[] fileList = dir.listFiles();
+			for (File file : fileList) {
+				if (file.isDirectory()) {
+					result += dirSize(file);
+				} else {
+					result += file.length();
+				}
+			}
+			return result;
+		}
+		return 0;
+	}
 
 	public static void cleanUp(File tempDir, String path) {
 		FileUtils.deleteDir(tempDir);
 
 		// delete zip file from download dir
 		File zip = new File(path);
-		if (!zip.delete()) {
-			Log.e(TAG, "cleanUp: File could not be deleted: " + zip.getAbsolutePath());
-		}
-
+		zip.delete();
 	}
 
 	public static String readFile(String file) throws IOException {
 		FileInputStream fstream = new FileInputStream(file);
-        return readFile(fstream);
+		return readFile(fstream);
 	}
 
 	public static String readFile(File file) throws IOException {
@@ -355,15 +321,15 @@ public class FileUtils {
 		if (mimeType == null) {
 			return false;
 		}
-        for (String s: MobileLearning.SUPPORTED_MEDIA_TYPES){
-            if(mimeType.equals(s)){
-                return true;
-            }
-        }
-        return false;
+		for (String s: MobileLearning.SUPPORTED_MEDIA_TYPES){
+			if(mimeType.equals(s)){
+				return true;
+			}
+		}
+		return false;
 	}
 
-    public static void moveFileToDir(File file, File mediaDir, boolean deleteOnError) {
+	public static void moveFileToDir(File file, File mediaDir, boolean deleteOnError) {
 		try {
 			org.apache.commons.io.FileUtils.moveFileToDirectory(file, mediaDir, true);
 		}catch (IOException e) {