70dc6a9a1ca4abfa9cb98d7707079e9ad4ed3f9a
Alex Little <alex@digital-campus.org>
2020-02-17 13:47:57 +0200

    OPPIA-238 - additional tests for multichoice questions

--- app/src/androidTest/java/quiz/models/MultiChoiceQuestionTest.java
+++ app/src/androidTest/java/quiz/models/MultiChoiceQuestionTest.java
@@ -12,7 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.assertFalse;
@@ -21,17 +20,24 @@ import static junit.framework.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class MultiChoiceQuestionTest {
 
-    private static final String MULTICHOICE_QUIZ_JSON = "quizzes/multichoice_nofeedback.json";
+    private static final String MULTICHOICE_NOFEEDBACK_JSON = "quizzes/multichoice_nofeedback.json";
+    private static final String MULTICHOICE_WITHFEEDBACK_JSON = "quizzes/multichoice_withfeedback.json";
     private static final String DEFAULT_LANG = "en";
-    private Quiz quiz;
+    private Quiz quizNoFeedback;
+    private Quiz quizWithFeedback;
 
     @Before
     public void setup() throws Exception {
         // Setting up before every test
-        String quizContent = Utils.FileUtils.getStringFromFile(
-                InstrumentationRegistry.getInstrumentation().getContext(), MULTICHOICE_QUIZ_JSON);
-        quiz = new Quiz();
-        quiz.load(quizContent, DEFAULT_LANG);
+        String quizNoFeedbackContent = Utils.FileUtils.getStringFromFile(
+                InstrumentationRegistry.getInstrumentation().getContext(), MULTICHOICE_NOFEEDBACK_JSON);
+        quizNoFeedback = new Quiz();
+        quizNoFeedback.load(quizNoFeedbackContent, DEFAULT_LANG);
+
+        String quizWithFeedbackContent = Utils.FileUtils.getStringFromFile(
+                InstrumentationRegistry.getInstrumentation().getContext(), MULTICHOICE_WITHFEEDBACK_JSON);
+        quizWithFeedback = new Quiz();
+        quizWithFeedback.load(quizWithFeedbackContent, DEFAULT_LANG);
     }
 
     /*
@@ -40,7 +46,7 @@ public class MultiChoiceQuestionTest {
     // correct answer
     @Test
     public void test_correctNoFeedback()throws Exception {
-        QuizQuestion quizQuestion = quiz.getCurrentQuestion();
+        QuizQuestion quizQuestion = quizNoFeedback.getCurrentQuestion();
         assertTrue(quizQuestion instanceof MultiChoice);
 
         // check before response added
@@ -72,15 +78,15 @@ public class MultiChoiceQuestionTest {
         assertEquals("Berlin", responseJson.get("text"));
 
         // check for whole quiz
-        assertEquals((float) 0, quiz.getUserscore());
-        quiz.mark(DEFAULT_LANG);
-        assertEquals((float) 1, quiz.getUserscore());
+        assertEquals((float) 0, quizNoFeedback.getUserscore());
+        quizNoFeedback.mark(DEFAULT_LANG);
+        assertEquals((float) 1, quizNoFeedback.getUserscore());
     }
 
     // incorrect answer (valid selection)
     @Test
     public void test_incorrectNoFeedback()throws Exception {
-        QuizQuestion quizQuestion = quiz.getCurrentQuestion();
+        QuizQuestion quizQuestion = quizNoFeedback.getCurrentQuestion();
         assertTrue(quizQuestion instanceof MultiChoice);
 
         ArrayList<String> userResponses = new ArrayList<>();
@@ -101,15 +107,15 @@ public class MultiChoiceQuestionTest {
         assertEquals("Bonn", responseJson.get("text"));
 
         // check for whole quiz
-        assertEquals((float) 0, quiz.getUserscore());
-        quiz.mark(DEFAULT_LANG);
-        assertEquals((float) 0, quiz.getUserscore());
+        assertEquals((float) 0, quizNoFeedback.getUserscore());
+        quizNoFeedback.mark(DEFAULT_LANG);
+        assertEquals((float) 0, quizNoFeedback.getUserscore());
     }
 
     // incorrect answer (invalid selection)
     @Test
     public void test_incorrectNoFeedbackInvalidOption()throws Exception {
-        QuizQuestion quizQuestion = quiz.getCurrentQuestion();
+        QuizQuestion quizQuestion = quizNoFeedback.getCurrentQuestion();
         assertTrue(quizQuestion instanceof MultiChoice);
 
         ArrayList<String> userResponses = new ArrayList<>();
@@ -130,14 +136,102 @@ public class MultiChoiceQuestionTest {
         assertEquals("somewhere", responseJson.get("text"));
 
         // check for whole quiz
-        assertEquals((float) 0, quiz.getUserscore());
-        quiz.mark(DEFAULT_LANG);
-        assertEquals((float) 0, quiz.getUserscore());
+        assertEquals((float) 0, quizNoFeedback.getUserscore());
+        quizNoFeedback.mark(DEFAULT_LANG);
+        assertEquals((float) 0, quizNoFeedback.getUserscore());
     }
 
     /*
     With feedback
      */
+
+    // correct answer
+
+    @Test
+    public void test_correctWithFeedback()throws Exception {
+        QuizQuestion quizQuestion = quizWithFeedback.getCurrentQuestion();
+        assertTrue(quizQuestion instanceof MultiChoice);
+
+        ArrayList<String> userResponses = new ArrayList<>();
+        userResponses.add("Red");
+        quizQuestion.setUserResponses(userResponses);
+        assertEquals("correct", quizQuestion.getFeedback(DEFAULT_LANG));
+        assertEquals((float) 1, quizQuestion.getUserscore());
+        assertEquals(100, quizQuestion.getScoreAsPercent());
+
+        // check json response object
+        JSONObject responseJson = quizQuestion.responsesToJSON();
+        assertTrue(responseJson.has("question_id"));
+        assertTrue(responseJson.has("score"));
+        assertTrue(responseJson.has("text"));
+
+        assertEquals(19769, responseJson.get("question_id"));
+        assertEquals(1.0, responseJson.get("score"));
+        assertEquals("Red", responseJson.get("text"));
+
+        // check for whole quiz
+        assertEquals((float) 0, quizWithFeedback.getUserscore());
+        quizWithFeedback.mark(DEFAULT_LANG);
+        assertEquals((float) 1, quizWithFeedback.getUserscore());
+    }
+    // incorrect answer (valid selection)
+    @Test
+    public void test_incorrectWithFeedback()throws Exception {
+        QuizQuestion quizQuestion = quizWithFeedback.getCurrentQuestion();
+        assertTrue(quizQuestion instanceof MultiChoice);
+
+        ArrayList<String> userResponses = new ArrayList<>();
+        userResponses.add("Black");
+        quizQuestion.setUserResponses(userResponses);
+        assertEquals("try again", quizQuestion.getFeedback(DEFAULT_LANG));
+        assertEquals((float) 0, quizQuestion.getUserscore());
+        assertEquals(0, quizQuestion.getScoreAsPercent());
+
+        // check json response object
+        JSONObject responseJson = quizQuestion.responsesToJSON();
+        assertTrue(responseJson.has("question_id"));
+        assertTrue(responseJson.has("score"));
+        assertTrue(responseJson.has("text"));
+
+        assertEquals(19769, responseJson.get("question_id"));
+        assertEquals(0.0, responseJson.get("score"));
+        assertEquals("Black", responseJson.get("text"));
+
+        // check for whole quiz
+        assertEquals((float) 0, quizWithFeedback.getUserscore());
+        quizWithFeedback.mark(DEFAULT_LANG);
+        assertEquals((float) 0, quizWithFeedback.getUserscore());
+    }
+
+    // incorrect answer (invalid selection)
+    @Test
+    public void test_incorrectWithFeedbackInvalid()throws Exception {
+        QuizQuestion quizQuestion = quizWithFeedback.getCurrentQuestion();
+        assertTrue(quizQuestion instanceof MultiChoice);
+
+        ArrayList<String> userResponses = new ArrayList<>();
+        userResponses.add("Purple");
+        quizQuestion.setUserResponses(userResponses);
+        // no feedback as invalid selection
+        assertEquals("", quizQuestion.getFeedback(DEFAULT_LANG));
+        assertEquals((float) 0, quizQuestion.getUserscore());
+        assertEquals(0, quizQuestion.getScoreAsPercent());
+
+        // check json response object
+        JSONObject responseJson = quizQuestion.responsesToJSON();
+        assertTrue(responseJson.has("question_id"));
+        assertTrue(responseJson.has("score"));
+        assertTrue(responseJson.has("text"));
+
+        assertEquals(19769, responseJson.get("question_id"));
+        assertEquals(0.0, responseJson.get("score"));
+        assertEquals("Purple", responseJson.get("text"));
+
+        // check for whole quiz
+        assertEquals((float) 0, quizWithFeedback.getUserscore());
+        quizWithFeedback.mark(DEFAULT_LANG);
+        assertEquals((float) 0, quizWithFeedback.getUserscore());
+    }
 
     /*
     Multilang, no feedback
