9cf930ad43853090ae041acf69064224181b5a3a
alex <consult@alexlittle.net>
2020-09-16 18:37:51 +0300

    update for new CHT package name

--- app/src/main/java/org/digitalcampus/oppia/utils/resources/ExternalResourceOpener.java
+++ app/src/main/java/org/digitalcampus/oppia/utils/resources/ExternalResourceOpener.java
@@ -16,7 +16,7 @@ import androidx.core.content.FileProvider;
 public class ExternalResourceOpener {
 
     private static final String FILEPROVIDER_AUTHORITY = BuildConfig.APPLICATION_ID  + ".provider";
-    public static final String EXTERNAL_APP_PACKAGE = "org.medicmobile.webapp.mobile";
+    public static final String EXTERNAL_APP_PACKAGE = "org.medicmobile.webapp.mobile.cht_cha_learning_care";
 
     private ExternalResourceOpener() {
         throw new IllegalStateException("Utility class");
