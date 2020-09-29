44ea6fcdeeb889791056d06535d177cc89e4cff0
Alex Little <alex@digital-campus.org>
2020-09-29 21:22:08 +0300

    additional model tests

--- app/src/main/java/org/digitalcampus/oppia/model/CustomValue.java
+++ app/src/main/java/org/digitalcampus/oppia/model/CustomValue.java
@@ -2,12 +2,9 @@ package org.digitalcampus.oppia.model;
 
 import androidx.annotation.NonNull;
 
-import java.util.Map;
-
 public class CustomValue<T> {
 
     private T value;
-
 
     public CustomValue(T value) {
         setValue(value);
@@ -22,14 +19,6 @@ public class CustomValue<T> {
             this.value = value;
         } else {
             throw new IllegalArgumentException("value must one of these types: String, int, float or boolean");
-        }
-    }
-
-    public static <V> V getSecureValue(Map<String, CustomValue> customFieldsMap, String key) {
-        if (customFieldsMap.containsKey(key)) {
-            return ((CustomValue<V>) customFieldsMap.get(key)).getValue();
-        } else {
-            return null;
         }
     }
 
