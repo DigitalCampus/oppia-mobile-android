7cb4967c7aae63b9fa2a1e8faa5b39b174419874
julio <jberzal86@gmail.com>
2021-05-06 18:44:23 +0200

    - all tests added

--- app/src/androidTest/java/androidTestFiles/UI/WelcomeUITest.java
+++ app/src/androidTest/java/androidTestFiles/UI/WelcomeUITest.java
@@ -81,4 +81,89 @@ public class WelcomeUITest extends MockedApiEndpointTest {
                 .perform(scrollTo())
                 .check(matches(isDisplayed()));
     }
+
+    @Test
+    public void resetPassword_goToLoginScreenAfterError() {
+
+        startServer(400, null, 0);
+
+        onView(withId(R.id.welcome_login))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.btn_reset_password))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.reset_username_field)).perform(typeText("username"));
+
+        onView(withId(R.id.reset_btn)).perform(click());
+
+        onView(withText(R.string.close)).perform(click());
+
+        onView(withId(R.id.reset_btn))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void rememberUsername_goToLoginScreenAfterSuccess() {
+
+        startServer(200, null, 0);
+
+        onView(withId(R.id.welcome_login))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.btn_remember_username))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.edit_email)).perform(typeText("valid@email.is"));
+
+        onView(withId(R.id.btn_remember_username)).perform(click());
+
+        onView(withText(R.string.ok)).perform(click());
+
+        onView(withId(R.id.login_btn))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void rememberUsername_showsErrorIfEmailPatterInvalid() {
+
+        startServer(200, null, 0);
+
+        onView(withId(R.id.welcome_login))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.btn_remember_username))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.edit_email)).perform(typeText("wrong-email"));
+
+        onView(withId(R.id.btn_remember_username)).perform(click());
+
+        onView(withText(R.string.error_register_email))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void rememberUsername_goToLoginScreenAfterError() {
+
+        startServer(400, null, 0);
+
+        onView(withId(R.id.welcome_login))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.btn_remember_username))
+                .perform(scrollTo(), click());
+
+        onView(withId(R.id.edit_email)).perform(typeText("valid@email.is"));
+
+        onView(withId(R.id.btn_remember_username)).perform(click());
+
+        onView(withText(R.string.close)).perform(click());
+
+        onView(withId(R.id.btn_remember_username))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+    }
 }
