529e4d4005e3cae53a7d74a14e5af7545872381f
julio <jberzal86@gmail.com>
2021-07-13 17:06:10 +0200

    - imports organized

--- app/src/main/java/org/digitalcampus/oppia/utils/custom_prefs/AdminEditTextPreference.java
+++ app/src/main/java/org/digitalcampus/oppia/utils/custom_prefs/AdminEditTextPreference.java
@@ -1,14 +1,9 @@
 package org.digitalcampus.oppia.utils.custom_prefs;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.util.AttributeSet;
 
 import androidx.preference.EditTextPreference;
-import androidx.preference.Preference;
-
-import org.digitalcampus.oppia.application.AdminSecurityManager;
 
 public class AdminEditTextPreference extends EditTextPreference {
 
