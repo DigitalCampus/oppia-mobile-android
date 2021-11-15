768b76b746c57718ef57e029d367464fc9c60adc
julio <jberzal86@gmail.com>
2021-11-15 14:29:21 +0100

    - Test updated

--- app/src/androidTest/java/androidTestFiles/BuildChecksOppiaCore.java
+++ app/src/androidTest/java/androidTestFiles/BuildChecksOppiaCore.java
@@ -74,6 +74,7 @@ public class BuildChecksOppiaCore {
         assertEquals(600, BuildConfig.SESSION_EXPIRATION_TIMEOUT);
 
         assertEquals(false, BuildConfig.SHOW_COURSE_DESCRIPTION);
+        assertEquals(true, BuildConfig.SHOW_GAMIFICATION_EVENTS);
 
 
         assertEquals("threshold", BuildConfig.GAMIFICATION_MEDIA_CRITERIA);
