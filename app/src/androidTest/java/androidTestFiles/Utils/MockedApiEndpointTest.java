fe80239178ad5f121a4d9438604606fa47020732
julio <jberzal86@gmail.com>
2021-12-14 13:42:10 +0100

    - added tests for submit and export trackers

--- app/src/androidTest/java/androidTestFiles/Utils/MockedApiEndpointTest.java
+++ app/src/androidTest/java/androidTestFiles/Utils/MockedApiEndpointTest.java
@@ -23,7 +23,7 @@ import static org.mockito.Mockito.when;
 public abstract class MockedApiEndpointTest extends DaggerInjectMockUITest {
 
     public static final String ERROR_MESSAGE_BODY = "responses/response_body_error_message.txt";
-    private static final String EMPTY_JSON = "{}";
+    protected static final String EMPTY_JSON = "{}";
 
     @Mock
     protected ApiEndpoint apiEndpoint;
