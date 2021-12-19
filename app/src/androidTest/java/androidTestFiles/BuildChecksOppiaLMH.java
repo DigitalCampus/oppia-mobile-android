cc4c64b4b94411c6855db6492a2200a26e118ed1
julio <jberzal86@gmail.com>
2021-12-19 13:50:51 +0100

    - Updated Build Checks for repository - moved custom values from default to custom file (update also in Drive)

--- app/src/androidTest/java/androidTestFiles/BuildChecksOppiaLMH.java
+++ app/src/androidTest/java/androidTestFiles/BuildChecksOppiaLMH.java
@@ -1,19 +1,20 @@
 package androidTestFiles;
 
-import static org.junit.Assert.assertEquals;
-
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
+import androidx.preference.PreferenceManager;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.digitalcampus.mobile.learning.BuildConfig;
 import org.digitalcampus.mobile.learning.R;
+import org.digitalcampus.oppia.application.App;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class BuildChecksOppiaLMH {
@@ -24,29 +25,35 @@ public class BuildChecksOppiaLMH {
     @Before
     public void setUp() throws Exception {
         context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        //We load the default prefs in case they were not loaded
+        App.loadDefaultPreferenceValues(context, false);
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
     @Test
     public void checkDefaultSettingsParameters() {
 
-
         String oppiaServerDefault = context.getString(R.string.prefServerDefault);
         String oppiaServerHost = context.getString(R.string.oppiaServerHost);
 
-        assertEquals(oppiaServerDefault, "https://cmedata.org/");
-        assertEquals(oppiaServerHost, "cmedata.org");
+        assertEquals("https://cmedata.org/", oppiaServerDefault);
+        assertEquals("cmedata.org", oppiaServerHost);
 
         assertEquals(false, BuildConfig.ADMIN_PROTECT_SETTINGS);
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_ADVANCED_SETTINGS);
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_SECURITY_SETTINGS);
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_SERVER);
         assertEquals(false, BuildConfig.ADMIN_PROTECT_ACTIVITY_SYNC);
         assertEquals(false, BuildConfig.ADMIN_PROTECT_ACTIVITY_EXPORT);
         assertEquals(true, BuildConfig.ADMIN_PROTECT_COURSE_DELETE);
         assertEquals(false, BuildConfig.ADMIN_PROTECT_COURSE_RESET);
         assertEquals(false, BuildConfig.ADMIN_PROTECT_COURSE_INSTALL);
         assertEquals(false, BuildConfig.ADMIN_PROTECT_COURSE_UPDATE);
-//        assertEquals("", BuildConfig.ADMIN_PROTECT_INITIAL_PASSWORD);
-        assertEquals(86, BuildConfig.ADMIN_PASSWORD_OVERRIDE_VERSION);
-
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_NOTIFICATIONS);
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_ENABLE_REMINDER_NOTIFICATIONS);
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_REMINDER_INTERVAL);
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_REMINDER_DAYS);
+        assertEquals(false, BuildConfig.ADMIN_PROTECT_REMINDER_TIME);
 
         assertEquals(true, BuildConfig.MENU_ALLOW_MONITOR);
         assertEquals(true, BuildConfig.MENU_ALLOW_SETTINGS);
@@ -56,6 +63,7 @@ public class BuildChecksOppiaLMH {
         assertEquals(true, BuildConfig.MENU_ALLOW_EDIT_PROFILE);
         assertEquals(true, BuildConfig.MENU_ALLOW_CHANGE_PASSWORD);
         assertEquals(false, BuildConfig.MENU_ALLOW_LANGUAGE);
+
         assertEquals(1, BuildConfig.DOWNLOAD_COURSES_DISPLAY);
 
         assertEquals(false, BuildConfig.START_COURSEINDEX_COLLAPSED);
@@ -68,22 +76,28 @@ public class BuildChecksOppiaLMH {
         assertEquals(true, BuildConfig.METADATA_INCLUDE_BATTERY_LEVEL);
 
         assertEquals(true, BuildConfig.OFFLINE_REGISTER_ENABLED);
+        assertEquals(true, BuildConfig.DELETE_ACCOUNT_ENABLED);
         assertEquals(false, BuildConfig.SESSION_EXPIRATION_ENABLED);
         assertEquals(600, BuildConfig.SESSION_EXPIRATION_TIMEOUT);
 
         assertEquals(false, BuildConfig.SHOW_COURSE_DESCRIPTION);
         assertEquals(true, BuildConfig.SHOW_GAMIFICATION_EVENTS);
 
-
         assertEquals("threshold", BuildConfig.GAMIFICATION_MEDIA_CRITERIA);
-
         assertEquals(80, BuildConfig.GAMIFICATION_DEFAULT_MEDIA_THRESHOLD);
+        assertEquals(false, BuildConfig.GAMIFICATION_MEDIA_SHOULD_REACH_END);
 
+        assertEquals("TIME_SPENT", BuildConfig.PAGE_COMPLETED_METHOD);
+        assertEquals(3, BuildConfig.PAGE_COMPLETED_TIME_SPENT);
+        assertEquals(125, BuildConfig.PAGE_COMPLETED_WPM);
 
+        assertEquals(true, BuildConfig.SHOW_GAMIFICATION_EVENTS);
         assertEquals("3", BuildConfig.GAMIFICATION_POINTS_ANIMATION);
         assertEquals(2, BuildConfig.DURATION_GAMIFICATION_POINTS_VIEW);
 
-
+        assertEquals("WEEKLY", BuildConfig.DEFAULT_REMINDER_INTERVAL);
+        assertEquals("10:30", BuildConfig.DEFAULT_REMINDER_TIME);
+        assertEquals("2", BuildConfig.DEFAULT_REMINDER_DAYS);
 
     }
 }
