57870fde50b794427791ac37d970300f2cbcae4f
Joseba S <jjoseba@gmail.com>
2024-08-26 23:08:02 +0200

    OPPIA-1647: Update JSInterface to take into account multiple audios

--- app/src/main/assets/js_injects/observe_audio_player.js
+++ app/src/main/assets/js_injects/observe_audio_player.js
@@ -1,10 +1,13 @@
 (function(){
-    var playButton = document.getElementById("play-icon");
-    playButton.addEventListener("click", function() {
-        var audioElement = document.querySelector("audio");
-        var audioSource = audioElement.getAttribute("src");
-        {{INTERFACE_EXPOSED_NAME}}.onPlayButtonClick(audioSource);
-        audioElement.addEventListener("ended", function() {
+    $(".audio-player-container, #audio-player-container").each(function(){
+        container = $(this);
+        var audioElem = container.find("audio")[0];
+        container.find(".play-icon,#play-icon").on('click', function(){
+            var audioSource = audioElem.getAttribute("src");
+            {{INTERFACE_EXPOSED_NAME}}.onPlayButtonClick(audioSource);
+        });
+        audioElem.addEventListener("ended", function() {
+            var audioSource = audioElem.getAttribute("src");
             {{INTERFACE_EXPOSED_NAME}}.onAudioCompleted(audioSource);
         });
     });
